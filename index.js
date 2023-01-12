dcf017cec19ff183975fd1c7423e469c280454c5
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-12 11:43:06 -0500

    objects lab phase-1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: ["George"],
+    streetAddress: ["123 Street"],
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+  
+    return obj;
+  }
+
+  function deleteFromEmployeeByKey(obj, key, value) {
+    return {
+      ...obj,
+      [key]: value,
+    };
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key];
+  
+    return employee;
+  }
+  
